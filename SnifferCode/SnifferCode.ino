9ad7caa4772beedaf2081958a941eb053535a7ea
Bart <1292740+bzuidgeest@users.noreply.github.com>
2022-10-29 22:16:04 +0200

    Code clean-up much more clean now. Seems to still work on the breadboard.

--- SnifferCode/SnifferCode.ino
+++ SnifferCode/SnifferCode.ino
@@ -1,24 +1,19 @@
-float freq = 15.0;  // default square wave frequency in kHz for testing the capacitors
-
-int voltageA0 = 0;
-int voltageA1 = 0;
-int resetADC;  // A2 grounded to reset ADC
-
-
 // pin assignments
 int pinButton = 10;  // defines D10 for pullup button switch
 int pinRelay = 3;
 int pinLedLeft = 5;
 int pinLedRight = 6;
-int pinSquareWave = 2;     // defines D2 for square wave output signal
+int pinSquareWave = 2;  // defines D2 for square wave output signal
 
-// defines D13 as the relay control pin
-int stateRelay = LOW;
 int stateButton;
 int previous = LOW;
 long time = 0;
 long debounce = 500;  // default of 500 ms works well
-int stayON = 1000;    // keeps relay on (HIGH) for 1000 ms or 1 second
+int stayON = 1000;    // keeps relay on (HIGH) for 1000 ms or 1 second faster switching might be acceptable.
+int voltageA0 = 0;
+int voltageA1 = 0;
+//int resetADC;  // A2 grounded to reset ADC
+float freq = 15.0;  // default square wave frequency in kHz for testing the capacitors
 
 void setup() {
   pinMode(pinButton, INPUT);  // defines D10 as an INPUT to activate the button switch
@@ -38,7 +33,7 @@ void setup() {
 
 void loop() {
   /*
-
+  // testing code
   digitalWrite(pinRelay, LOW);
   digitalWrite(pinLedLeft, HIGH);
   digitalWrite(pinLedRight, LOW);
@@ -50,65 +45,53 @@ void loop() {
   Serial.println("B");
   delay(stayON);
   */
-  
-  
 
   stateButton = digitalRead(pinButton);
   if (stateButton == HIGH && previous == LOW & millis() - time > debounce) {
-    if (stateRelay == HIGH) {
-      Serial.println("B");
-      digitalWrite(pinRelay, LOW);
-    } else {
-      Serial.println("A");
-      digitalWrite(pinRelay, HIGH);
-      delay(stayON);
 
-      resetADC = analogRead(A2);  // A2 is grounded to clear the ADC before reading A0 & A1
+    // Print a message
+    Serial.println("Testing...");
 
-      int sensorValue = analogRead(A0);
-      float voltageA0 = sensorValue * (5.0 / 1023.0);
-      Serial.print("A0: ");
-      Serial.println(voltageA0, 4);
+    // Clear leds
+    digitalWrite(pinLedLeft, LOW);   // D6 off - (left LED)
+    digitalWrite(pinLedRight, LOW);  // D5 on - (right LED)
+    delay(stayON);
 
-      if (voltageA0 > voltageA1) {
-        digitalWrite(pinLedLeft, LOW);  // D6 off - (left facing LED)
-      }
-      else if (voltageA0 < voltageA1) {
-        digitalWrite(pinLedLeft, HIGH); // D6 on - (left facing LED) to indicate Foil Side of Capacitor
-      }
-      if (voltageA0 > voltageA1) {
-        digitalWrite(pinLedRight, HIGH); // D5 on - (right facing LED) to indicate Foil Side of Capacitor
-      }
-      else if (voltageA0 < voltageA1) {
-        digitalWrite(pinLedRight, LOW); // D5 off - (right facing LED)
-      }
-      digitalWrite(pinRelay, LOW); // D13 off 
+    //turn on relais
+    digitalWrite(pinRelay, HIGH);
 
-      delay(100); // small delay to reduce rollover samples
+    // Let things stabilise
+    delay(stayON);
 
-      int sensorValue1 = analogRead(A1);
-      float voltageA1 = sensorValue1 * (5.0 / 1023.0);
-      Serial.print("A1: ");
-      Serial.println(voltageA1, 4);
+    // Clear ADC -> is this really needed?
+    //resetADC = analogRead(A2);  // A2 is grounded to clear the ADC before reading A0 & A1
 
-      if (voltageA0 > voltageA1) {
-        digitalWrite(pinLedLeft, LOW); // D6 off - (left facing LED)
-      }
-      else if (voltageA0 < voltageA1) {
-        digitalWrite(pinLedLeft, HIGH); // D6 on - (left facing LED) to indicate Foil Side of Capacitor
-      }
-      if (voltageA0 > voltageA1) {
-        digitalWrite(pinLedRight, HIGH); // D5 on - (right side facing LED) to indicate Foil Side of Capacitor
-      }
-      else if (voltageA0 < voltageA1) {
-        digitalWrite(pinLedRight, LOW); // D5 off - (right facing LED)
-      }
+    // Measure signal strength
+    float voltageA0 = analogRead(A0) * (5.0 / 1023.0);
+    Serial.print("Value0: ");
+    Serial.println(voltageA0, 4);
+
+    // turn off relais
+    digitalWrite(pinRelay, LOW);  // D13 off
+
+    // Let things stabilise
+    delay(stayON);
+
+    // Measure signal strength
+    float voltageA1 = analogRead(A0) * (5.0 / 1023.0);
+    Serial.print("Value1: ");
+    Serial.println(voltageA1, 4);
+
+    // Show the result.
+    if (voltageA0 > voltageA1) {
+      digitalWrite(pinLedLeft, LOW);    // D6 off - (left LED)
+      digitalWrite(pinLedRight, HIGH);  // D5 on - (right LED) to indicate Foil Side of Capacitor
+    } else if (voltageA0 < voltageA1) {
+      digitalWrite(pinLedLeft, HIGH);  // D6 on - (left LED) to indicate Foil Side of Capacitor
+      digitalWrite(pinLedRight, LOW);  // D5 off - (right LED)
     }
+
     time = millis();
   }
   previous == stateButton;
-
-  //tone(pinSquareWave, freq * 1000);
-
-  
 }
